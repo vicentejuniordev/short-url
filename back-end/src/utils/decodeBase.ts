393b021521cc086771b458e3a6714172103764c8
vicenteprogrammer <vicentejunior4522@gmail.com>
2025-10-24 09:18:32 -0300

    feat: implemented decode function

--- back-end/src/utils/decodeBase.ts
+++ back-end/src/utils/decodeBase.ts
@@ -5,11 +5,9 @@ const decode = async (slug: string)=>{
     const slugSplit = slug.split('').reverse();
     for(let i = slugSplit.length; i > 0; i--){
         const ch = slugSplit[i - 1];
-        console.log(ch);
         if (ch === undefined) continue;
         let index = characters.indexOf(ch); 
         slugDecode += index * Math.pow(62, i - 1)
-        console.log(slugDecode)
     }
     return slugDecode
 }
